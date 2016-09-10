a41267ce6ddbb7b0d8d10c0539335500d3bc22b8
JP Erasmus <jperasmus11@gmail.com>
2016-09-10 13:40:02 +0200

    Ping command

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -14,52 +14,48 @@ const msgDefaults = {
 const handler = (payload, res) => {
   const host = payload.text || '';
   
-  // tcpp.ping({ address: host, port: 80, attempts: 1, timeout: 1000 }, (err, data) => {
-  //   if (err) {
-  //     res.set('content-type', 'application/json');
-  //     return res.status(200).json({
-  //       msg: ':poop:, something went wrong while pinging `' + host + '`',
-  //       error: err
-  //     });
-  //   }
-  //   const deets = _.reduce(data.results, (current, next) => {
-  //     return current += `${next.seq} => ${next.err ? next.err : ''}${next.time ? next.time + ' ms' : ''}\n`
-  //   }, '');
-  //   const text = `Attempts: ${data.attempts}\nAvg time: ${data.avg} ms\nMax time: ${data.max} ms\nMin time: ${data.min} ms\nDeets: ${deets}`;
-  //   const attachments = {
-  //     title: `Host: ${data.address}`,
-  //     title_link: data.address,
-  //     text,
-  //     mrkdwn_in: ['text', 'pretext']
-  //   };
-  //
-  //   const msg = _.defaults({
-  //     channel: payload.channel_name,
-  //     title: 'Testing 123',
-  //     attachments: {
-  //       title_link: 'https://jperasmus.me',
-  //       text: 'text goes here',
-  //       markdwn_in: ['text', 'pretext']
-  //     }
-  //   }, msgDefaults);
-  //
-  //   res.set('content-type', 'application/json');
-  //   res.status(200).json(msg);
-  //   return;
-  // });
+  tcpp.ping({ address: host, port: 80, attempts: 1, timeout: 1000 }, (err, data) => {
+    if (err) {
+      res.set('content-type', 'application/json');
+      return res.status(200).json({
+        msg: ':poop:, something went wrong while pinging `' + host + '`',
+        error: err
+      });
+    }
+    const deets = _.reduce(data.results, (current, next) => {
+      return current += `${next.seq} => ${next.err ? next.err : ''}${next.time ? next.time + ' ms' : ''}\n`
+    }, '');
+    const text = `Attempts: ${data.attempts}\nAvg time: ${data.avg} ms\nMax time: ${data.max} ms\nMin time: ${data.min} ms\nDeets: ${deets}`;
+    const attachments = [{
+      title: `Host: ${data.address}`,
+      title_link: data.address,
+      text,
+      mrkdwn_in: ['text', 'pretext']
+    }];
+
+    const msg = _.defaults({
+      channel: payload.channel_name,
+      title: 'Pinging ' + host,
+      attachments
+    }, msgDefaults);
+
+    res.set('content-type', 'application/json');
+    res.status(200).json(msg);
+    return;
+  });
   
-  const msg = _.defaults({
-    channel: payload.channel_name,
-    title: 'Pinging ' + host,
-    attachments: [{
-      title_link: 'https://jperasmus.me',
-      text: 'text goes here',
-      markdwn_in: ['text', 'pretext']
-    }]
-  }, msgDefaults);
-  
-  res.set('content-type', 'application/json');
-  res.status(200).json(msg);
+  // const msg = _.defaults({
+  //   channel: payload.channel_name,
+  //   attachments: [{
+  //     title: 'Pinging ' + host,
+  //     title_link: 'https://jperasmus.me',
+  //     text: 'text goes here',
+  //     markdwn_in: ['text', 'pretext']
+  //   }]
+  // }, msgDefaults);
+  //
+  // res.set('content-type', 'application/json');
+  // res.status(200).json(msg);
 };
 
 module.exports = { pattern: /ping/ig, handler: handler };
