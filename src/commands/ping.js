69b6b065c238f808787723232cf477cf5ecea1b8
JP Erasmus <jperasmus11@gmail.com>
2016-09-10 10:25:12 +0200

    Ping command

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -2,7 +2,7 @@
 
 const _ = require('lodash')
 const config = require('../config')
-const trending = require('github-trending')
+const ping = require('ping')
 
 const msgDefaults = {
   response_type: 'in_channel',
@@ -11,27 +11,31 @@ const msgDefaults = {
 }
 
 const handler = (payload, res) => {
-  trending('javascript', (err, repos) => {
-    if (err) throw err
-
-    var attachments = repos.slice(0, 5).map((repo) => {
-      return {
-        title: `${repo.owner}/${repo.title} `,
-        title_link: repo.url,
-        text: `_${repo.description}_\n${repo.language} • ${repo.star}>`,
+  const host = payload.text || 'google.com'
+  
+  ping.promise.probe(host)
+    .then((res) => {
+      console.log(res)
+      var attachments = {
+        title: `${res.alive} - ${res.host} [${res.time}] `,
+        title_link: res.host,
+        text: `${res.output}`,
         mrkdwn_in: ['text', 'pretext']
       }
+  
+      let msg = _.defaults({
+        channel: payload.channel_name,
+        attachments: attachments
+      }, msgDefaults)
+  
+      res.set('content-type', 'application/json')
+      res.status(200).json(msg)
+      return
     })
-
-    let msg = _.defaults({
-      channel: payload.channel_name,
-      attachments: attachments
-    }, msgDefaults)
-
-    res.set('content-type', 'application/json')
-    res.status(200).json(msg)
-    return
-  })
+    .catch((err) => {
+      res.set('content-type', 'application/json')
+      res.status(500).json({ msg: 'Poop, something went wrong with the `ping`.'})
+    })
 }
 
 module.exports = { pattern: /repos/ig, handler: handler }
